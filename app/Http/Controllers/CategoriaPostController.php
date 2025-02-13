45bd61c3f9dd5835e748cd9b0d075209170333f5
Diogo Cardoso <digovegeta@gmail.com>
2025-02-13 19:38:49 -0300

    Correção nome da coluna

--- app/Http/Controllers/CategoriaPostController.php
+++ app/Http/Controllers/CategoriaPostController.php
@@ -32,13 +32,13 @@ class CategoriaPostController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' =>'required',
+            'nome' =>'required',
             'descricao' =>'required'
         ]);
 
         $cps = new CategoriaPost();
         
-        $cps->name = $request->input('name');
+        $cps->nome = $request->input('nome');
         $cps->descricao = $request->input('descricao');
         $cps->save();
         return view('categoria_post.create');
@@ -72,13 +72,13 @@ class CategoriaPostController extends Controller
     public function update(Request $request, string $id)
     {
         $request->validate([
-            'name' =>'required',
+            'nome' =>'required',
             'descricao' =>'required'
         ]);
 
         $cps = CategoriaPost::findOrFail($id);
         
-        $cps->name = $request->input('name');
+        $cps->nome = $request->input('nome');
         $cps->descricao = $request->input('descricao');
         $cps->update();
         return view('categoria_post.index');
