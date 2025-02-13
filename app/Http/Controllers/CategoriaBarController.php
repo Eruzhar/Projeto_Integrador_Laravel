6daca8f9051437f50a9eedf19079c323097a92ec
Tiago Menezes <tmenezes2024@icloud.com>
2025-02-13 16:27:03 -0300

    Atualização de Controller Bar e Evento

--- app/Http/Controllers/CategoriaBarController.php
+++ app/Http/Controllers/CategoriaBarController.php
@@ -3,8 +3,94 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\CategoriaBar;
 
 class CategoriaBarController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $cbs= CategoriaBar::all();
+        return view("categoria_post.index", [
+            "categoria_posts" => $cbs
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view("categoria_bar.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' =>'required',
+            'descricao' =>'required'
+        ]);
+
+        $cbs = new CategoriaBar();
+        
+        $cbs->name = $request->input('name');
+        $cbs->descricao = $request->input('descricao');
+        $cbs->save();
+        return view('categoria_bar.create');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $cbs = CategoriaBar::findOrFail($id);
+        return view("categoria_bar.show", [
+            "categoria_bar"=> $cbs
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $cbs = CategoriaPost::findOrFail($id);
+        return view("categoria_bar.edit", [
+            "categoria_bar"=> $cbs
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'name' =>'required',
+            'descricao' =>'required'
+        ]);
+
+        $cbs = CategoriaBar::findOrFail($id);
+        
+        $cbs->name = $request->input('name');
+        $cbs->descricao = $request->input('descricao');
+        $cbs->update();
+        return view('categoria_bar.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $cbs = CategoriaBar::findOrFail($id);
+        $cbs->delete();
+        return view("categoria_bar.index");
+    }
 }
--- app/Http/Controllers/CategoriaEventoController.php
+++ app/Http/Controllers/CategoriaEventoController.php
@@ -3,8 +3,96 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\CategoriaEvento;
+use View;
+
 
 class CategoriaEventoController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $ces = CategoriaEvento ::all();
+        return view("categoria_evento", [
+            "categoria_evento" => $ces
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view("categoria_evento.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' =>'required',
+            'descricao' =>'required'
+        ]);
+
+        $ces = new CategoriaEvento();
+        
+        $ces->name = $request->input('name');
+        $ces->descricao = $request->input('descricao');
+        $ces->save();
+        return View('categoria_evento.create');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $ces = Categoriaces::findOrFail($id);
+        return view("categoria_post.show", [
+            "categoria_post"=> $ces
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $ces = CategoriaEvento::findOrFail($id);
+        return view("categoria_post.edit", [
+            "categoria_post"=> $ces
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'name' =>'required',
+            'descricao' =>'required'
+        ]);
+
+        $ces = CategoriaEvento::findOrFail($id);
+        
+        $ces->name = $request->input('name');
+        $ces->descricao = $request->input('descricao');
+        $ces->update();
+        return redirect()->route('categoria_evento.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $ces = CategoriaEvento::findOrFail($id);
+        $ces->delete();
+        return view("categoria_evento.index");
+    }
 }
