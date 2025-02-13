4a06305d887eca3d27b8b6419ad4de5dccd00fca
Diogo Cardoso <digovegeta@gmail.com>
2025-02-13 19:14:11 -0300

    Inclusão codigo para inserir dados no banco

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Profissao;
+use App\Models\CategoriaBar;
+use App\Models\CategoriaEvento;
+use App\Models\CategoriaPost;
+use App\Models\StatusCotacao;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -37,6 +41,54 @@ class DatabaseSeeder extends Seeder
         Profissao::factory()->create([
             'name'=> 'Garçom',
             'descricao' => 'GarçomDesc',
+        ]);
+        StatusCotacao::factory()->create([
+            'name'=> 'Novo',
+            'descricao' => 'NovoDesc',
+        ]);
+        StatusCotacao::factory()->create([
+            'name'=> 'Pendente',
+            'descricao' => 'PendenteDesc',
+        ]);
+        StatusCotacao::factory()->create([
+            'name'=> 'Concluído',
+            'descricao' => 'ConcluídoDesc',
+        ]);
+        CategoriaBar::factory()->create([
+            'name'=> 'Bar 1',
+            'descricao' => 'Bar 1Desc',
+        ]);
+        CategoriaBar::factory()->create([
+            'name'=> 'Bar 2',
+            'descricao' => 'Bar 2Desc',
+        ]);
+        CategoriaBar::factory()->create([
+            'name'=> 'Bar 3',
+            'descricao' => 'Bar 3Desc',
+        ]);
+        CategoriaPost::factory()->create([
+            'name'=> 'Bar',
+            'descricao' => 'BarDesc',
+        ]);
+        CategoriaPost::factory()->create([
+            'name'=> 'Drink',
+            'descricao' => 'DrinkDesc',
+        ]);
+        CategoriaPost::factory()->create([
+            'name'=> 'Evento',
+            'descricao' => 'EventoDesc',
+        ]);
+        CategoriaEvento::factory()->create([
+            'name'=> 'Evento 1',
+            'descricao' => 'Evento 1Desc',
+        ]);
+        CategoriaEvento::factory()->create([
+            'name'=> 'Evento 2',
+            'descricao' => 'Evento 2Desc',
+        ]);
+        CategoriaEvento::factory()->create([
+            'name'=> 'Evento 3',
+            'descricao' => 'Evento 3Desc',
         ]);
 
     }
