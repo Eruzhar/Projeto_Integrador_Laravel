3b821ab6d55b9a452ca0b26119d8039d21efe530
Eric Vostal Hausner <evhausner@gmail.com>
2025-02-13 17:21:13 -0300

    Home iniciada

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,9 @@
-<x-layout title="Clube Bar" style="css/home.css">
-    
+<x-layout title="Clube Bar">
+    @vite(['resources/css/home.css'])
+    <x-header></x-header>
+    <section class="home-main">
+        
+    </section>
+    <section></section>
+    <x-footer></x-footer>
 </x-layout>
