cb5cd47b786136b72993a586ef990d586130ade4
Eric Vostal Hausner <evhausner@gmail.com>
2025-02-14 14:53:32 -0300

    Atualizada página Home2

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,14 +1,14 @@
 @vite(['resources/css/footer.css'])
 <footer>
     <div class="d-grid footer-container">                        
-        <p><a href="" class="footer-link c1">Política de Privacidade</a></p>
-        <p><a href="" class="footer-link c1">Política de Cookies</a></p>
+        <p class="footer-item"><a href="" class="footer-link c1">Política de Privacidade</a></p>
+        <p class="footer-item"><a href="" class="footer-link c1">Política de Cookies</a></p>
         <div class="vr c2"></div>
-        <p><a href="" class="footer-link c3">Termos e condições</a></p>
-        <p><a href="" class="footer-link c3">Política de Cookies</a></p>        
+        <p class="footer-item"><a href="" class="footer-link c3">Termos e condições</a></p>
+        <p class="footer-item"><a href="" class="footer-link c3">Política de Cookies</a></p>        
         <div class="vr c4"></div>
-        <p><a href="" class="footer-link c5">Sobre nós</a></p>
-        <p><a href="" class="footer-link c5">Perguntas Frequentes</a></p>
+        <p class="footer-item"><a href="" class="footer-link c5">Sobre nós</a></p>
+        <p class="footer-item"><a href="" class="footer-link c5">Perguntas Frequentes</a></p>
     </div>
     
     <div class="copyright">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,8 @@
 <x-layout title="Clube Bar">
     @vite(['resources/css/home.css'])
     <x-header></x-header>
-    <section class="home-main">
-        
-    </section>
-    <section class="home-sub">
+    <section class="home-main"></section>    
+    <section class="home-sub">        
         <h1 class="home-sub-text">Uma experiência sensorial única, que vai além do sabor.</h1>
     </section>
     <x-footer></x-footer>
