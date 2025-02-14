cadcfde0197b1031947d2d5afa6006beed938179
Eric Vostal Hausner <evhausner@gmail.com>
2025-02-14 16:29:08 -0300

    Formulario Curriculo cadastra na database

--- app/Models/Curriculo.php
+++ app/Models/Curriculo.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\Profissao;
 class Curriculo extends Model
 {
+    use HasFactory;
     protected $table = 'curriculo';
+
+    protected $fillable = ['nome', 'arquivo', 'informacoes', 'profissao_id'];
 
     public function trabalho(){
         return $this->hasOne(Profissao::class, 'profissao_id');
