458077b3ce39606f4f5c9a86b42caba8aca90ddd
Diogo Cardoso <digovegeta@gmail.com>
2025-02-14 14:52:55 -0300

    ajustes

--- resources/views/components/layout-dashboard.blade.php
+++ resources/views/components/layout-dashboard.blade.php
@@ -2,7 +2,7 @@
 @vite(['{{$css}}']);
 <!DOCTYPE html>
 <html lang="pt">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
@@ -12,13 +12,13 @@
   </head>
   <body>
     {{$slot}}
-  </section>
-      <script    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"    crossorigin="anonymous"    ></script>
-      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-      <script>
-        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-        </script>
-        <script src="../JS/Buttons-Dashbords.js"></script>
-</body>
+    </section>
+    <script    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"    crossorigin="anonymous"    ></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+    <script>
+      const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+      const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+      </script>
+      <script src="../JS/Buttons-Dashbords.js"></script>
+  </body>
 </html>
