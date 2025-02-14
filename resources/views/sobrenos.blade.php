bb9adb19dd76fe368caa3206c4e617b97713aaf8
Eric Vostal Hausner <evhausner@gmail.com>
2025-02-14 17:22:35 -0300

    Sobre nos em criacao

--- resources/views/sobrenos.blade.php
+++ resources/views/sobrenos.blade.php
@@ -1,4 +1,13 @@
 <x-layout title="Sobre Nós">
+    @vite('resources/css/sobrenos.css')
     <x-header></x-header>
-    
+    <section class="sn-main">
+        <h1 class="sn-h1">Sobre Nós</h1>
+        <p class="sn-texto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis, nunc at interdum scelerisque, ipsum velit ultricies dolor, vel bibendum nunc nunc vel est. Sed ac quam vel nisl gravida consectetur. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed id consectetur ligula, a pulvinar neque. Nulla facilisi. Quisque vel justo vel libero bibendum consectetur. Sed in orci vel felis faucibus fermentum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis, nunc at interdum scelerisque, ipsum velit ultricies dolor, vel bibendum nunc nunc vel est. Sed ac quam vel nisl gravida consectetur. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed id consectetur ligula, a pulvinar neque. Nulla facilisi. Quisque vel justo vel libero bibendum consectetur. Sed in orci vel felis faucibus fermentum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis, nunc at interdum scelerisque, ipsum velit ultricies dolor, vel bibendum nunc nunc vel est. Sed ac quam vel nisl gravida consectetur. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed id consectetur ligula, a pulvinar neque. Nulla facilisi. Quisque vel justo vel libero bibendum consectetur. Sed in orci vel felis faucibus fermentum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis, nunc at interdum scelerisque, ipsum velit ultricies dolor, vel bibendum nunc nunc vel est. Sed ac quam vel nisl gravida consectetur. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed id consectetur ligula, a pulvinar neque. Nulla facilisi. Quisque vel justo vel libero bibendum consectetur. Sed in orci vel felis faucibus fermentum.
+        </p>
+    </section>
+    <section class="sn-sub"></section>
 </x-layout>
