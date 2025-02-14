9a6fefedbac252362607ba5ea7873cdf4af2436d
Diogo Cardoso <digovegeta@gmail.com>
2025-02-14 14:26:04 -0300

    adção de rota em buttons

--- resources/views/galeriaDashboard.blade.php
+++ resources/views/galeriaDashboard.blade.php
@@ -4,30 +4,30 @@
         <div class="Cabeçalho">
             <h1>Dahsboard-Galeria</h1>
             <div>
-                <a href='../HTML/Dashboard-Menu.html'>
+                <a href='{{route('menu')}}'>
                     <x-button-voltar descricao="Voltar">
-                        </x-button-voltar>
-                    </a>
-                    <a href='../HTML/AdcionarItens-Galeria.html'>
-                        <x-button-novo descricao="Adicionar novo">
-                            </x-button-novo>
-                        </a>                
-                    </div>
-                </div>
-                <div class="Filtro">
-                    <h4>Ordem:</h4>
-                    <select class="form-control" id="exampleFormControlSelect1">
-                        <option>Nome</option>
-                        <option>Data</option>
-                        <option>Tamanho</option>
-                    </select>
-                    <h4>Exibição:</h4>
-                    <select class="form-control" id="exampleFormControlSelect1">
-                        <option>Lista</option>
-                        <option>Galeria</option>
-                    </select>
-                </div>
-                <div class="DB">       
-                    </div>
-                </section>
+                    </x-button-voltar>
+                </a>
+                <a href='{{route('galeriaNovo')}}'>
+                    <x-button-novo descricao="Adicionar novo">
+                    </x-button-novo>
+                </a>                
+            </div>
+        </div>
+    <div class="Filtro">
+        <h4>Ordem:</h4>
+        <select class="form-control" id="exampleFormControlSelect1">
+            <option>Nome</option>
+            <option>Data</option>
+            <option>Tamanho</option>
+        </select>
+        <h4>Exibição:</h4>
+        <select class="form-control" id="exampleFormControlSelect1">
+            <option>Lista</option>
+            <option>Galeria</option>
+        </select>
+    </div>
+    <div class="DB">       
+        </div>
+    </section>
 </x-layout-dashboard><!DOCTYPE html>
