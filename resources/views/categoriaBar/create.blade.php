0b0463a8e86eacf7fd73776ce1f31dc77bcdd508
Diogo Cardoso <digovegeta@gmail.com>
2025-02-13 22:00:53 -0300

    Componentes para script, botões e cards

--- resources/views/categoriaBar/create.blade.php
+++ resources/views/categoriaBar/create.blade.php
@@ -3,6 +3,15 @@
 <h1>
 create categoria bar
 </h1>
-
-
+<x-card-dashboard descricao="4321" titulo="titulo" id="1" img="https://placehold.co/600x400">
+    <x-button-edit descricao="4321">123</x-button-edit>
+    <x-button-voltar descricao="43210"></x-button-voltar>
+    <x-button-limpar descricao="432102"></x-button-limpar>
+    <x-button-enviar descricao="432101"></x-button-enviar>
+    <x-button-novo descricao="432103"></x-button-novo>
+    <x-button-ocultar descricao="432104" id="1" display="bloc"></x-button-ocultar>
+    <x-button-visivel descricao="432105" id="1" display="bloc"></x-button-visivel>
+    <x-button-apagar descricao="432106" id="1"></x-button-apagar>
+</x-card-dashboard>
 </x-layout>
+<x-script></x-script>
