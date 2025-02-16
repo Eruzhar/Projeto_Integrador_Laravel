218e22d1b3ebf0b64f14f1dce9f826e35c71250b
Diogo Cardoso <digovegeta@gmail.com>
2025-02-15 21:17:47 -0300

    Seed independente para Cotação

---
+++ database/seeders/CotacaoSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Cotacao;
+
+class CotacaoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Cotacao::factory()->create([
+            'nome'=> 'João',
+            'data_evento' => now(),
+            'telefone' => '71 - 9 9999-9999',
+            'email' => 'joao@example.com',
+            'localidade' => 'Rio de Janeiro',
+            'observacoes' => 'Teste de cotação',
+            'qtd_convidados' => '20',
+            'status_cotacao_id' => '1',
+            'categoria_bar_id' => '1',
+            'categoria_evento_id' => '1'
+        ]);
+        Cotacao::factory()->create([
+            'nome'=> 'João',
+            'data_evento' => now(),
+            'telefone' => '71 - 9 9999-9999',
+            'email' => 'joao@example.com',
+            'localidade' => 'Rio de Janeiro',
+            'observacoes' => 'Teste de cotação',
+            'qtd_convidados' => '20',
+            'status_cotacao_id' => '2',
+            'categoria_bar_id' => '1',
+            'categoria_evento_id' => '1'
+        ]);
+        Cotacao::factory()->create([
+            'nome'=> 'João',
+            'data_evento' => now(),
+            'telefone' => '71 - 9 9999-9999',
+            'email' => 'joao@example.com',
+            'localidade' => 'Rio de Janeiro',
+            'observacoes' => 'Teste de cotação',
+            'qtd_convidados' => '20',
+            'status_cotacao_id' => '3',
+            'categoria_bar_id' => '1',
+            'categoria_evento_id' => '1'
+        ]);
+    }
+}
