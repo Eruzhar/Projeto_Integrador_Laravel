b010b821b17eb8f033602ccca560dab44ae621a3
Diogo Cardoso <digovegeta@gmail.com>
2025-02-15 21:16:38 -0300

    inclusão de inserts no banco

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Http\Controllers\CotacaoController;
 use App\Models\User;
 use App\Models\Profissao;
 use App\Models\CategoriaBar;
 use App\Models\CategoriaEvento;
 use App\Models\CategoriaPost;
 use App\Models\StatusCotacao;
+use App\Models\Cotacao;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -89,6 +91,42 @@ class DatabaseSeeder extends Seeder
         CategoriaEvento::factory()->create([
             'nome'=> 'Evento 3',
             'descricao' => 'Evento 3Desc',
+        ]);
+        Cotacao::factory()->create([
+            'nome'=> 'João',
+            'data_evento' => now(),
+            'telefone' => '71 - 9 9999-9999',
+            'email' => 'joao@example.com',
+            'localidade' => 'Rio de Janeiro',
+            'observacoes' => 'Teste de cotação',
+            'qtd_convidados' => '20',
+            'status_cotacao_id' => '1',
+            'categoria_bar_id' => '1',
+            'categoria_evento_id' => '1'
+        ]);
+        Cotacao::factory()->create([
+            'nome'=> 'João',
+            'data_evento' => now(),
+            'telefone' => '71 - 9 9999-9999',
+            'email' => 'joao@example.com',
+            'localidade' => 'Rio de Janeiro',
+            'observacoes' => 'Teste de cotação',
+            'qtd_convidados' => '20',
+            'status_cotacao_id' => '2',
+            'categoria_bar_id' => '1',
+            'categoria_evento_id' => '1'
+        ]);
+        Cotacao::factory()->create([
+            'nome'=> 'João',
+            'data_evento' => now(),
+            'telefone' => '71 - 9 9999-9999',
+            'email' => 'joao@example.com',
+            'localidade' => 'Rio de Janeiro',
+            'observacoes' => 'Teste de cotação',
+            'qtd_convidados' => '20',
+            'status_cotacao_id' => '3',
+            'categoria_bar_id' => '1',
+            'categoria_evento_id' => '1'
         ]);
 
     }
