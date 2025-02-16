cf564202bf500d0754aaf9ce66686f4ca954c5f7
Diogo Cardoso <digovegeta@gmail.com>
2025-02-15 21:25:13 -0300

    inclusao do factory no model

--- app/Models/Cotacao.php
+++ app/Models/Cotacao.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\StatusCotacao;
 use App\Models\CategoriaBar;
 use App\Models\CategoriaEvento;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Cotacao extends Model
 {
+    use HasFactory;
     protected $table = 'cotacao';
 
     public function categoria_status(){
