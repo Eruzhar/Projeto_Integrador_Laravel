d20bbbff8aefef53095a0dccbcda4a477600cc4b
Eric Vostal Hausner <evhausner@gmail.com>
2025-02-18 16:47:15 -0300

    Card mod1

--- resources/views/components/drink_card.blade.php
+++ resources/views/components/drink_card.blade.php
@@ -2,7 +2,7 @@
 @vite(['resources/css/drink_card.css'])
 <div class="drink_card border rounded">    
     {{-- <img src="{{asset('img/drinks/'.$arquivo)}}" alt="" class="card-img"> --}}
-    <img src="{{asset('../../public/img/drinks/'.$arquivo)}}" alt="" class="card-img">    
+    <img src="{{url('../../public/img/drinks/'.$arquivo)}}" alt="" class="card-img">    
     <div class="card-header">
         <h1 class="card-name">{{ $titulo }}</h1>
         <h2 class="card-desc">{{ $descricao }}</h2>
