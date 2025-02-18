a4837c610ae78490690c9849eb9b87f0b993eeb3
Diogo Cardoso <digovegeta@gmail.com>
2025-02-18 10:56:25 -0300

    Criação de Seeder para chaves de tabelas necessarias para funcionamento do sistema

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\CotacaoController;
 use App\Models\Curriculo;
 use App\Models\User;
 use App\Models\Profissao;
-use App\Models\CategoriaBar;
-use App\Models\CategoriaEvento;
-use App\Models\CategoriaPost;
-use App\Models\StatusCotacao;
 use App\Models\Cotacao;
 use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -29,132 +25,39 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);*/
-
-        Profissao::factory()->create([
-            'nome'=> 'Bartender',
-            'descricao' => 'BartenderDesc',
-        ]);        
-        Profissao::factory()->create([
-            'nome'=> 'Backbar',
-            'descricao' => 'BackbarDesc',
-        ]);
-        Profissao::factory()->create([
-            'nome'=> 'Copeira',
-            'descricao' => 'CopeiraDesc',
-        ]);
-        Profissao::factory()->create([
-            'nome'=> 'Garçom',
-            'descricao' => 'GarçomDesc',
-        ]);
-        StatusCotacao::factory()->create([
-            'nome'=> 'Novo',
-            'descricao' => 'NovoDesc',
-        ]);
-        StatusCotacao::factory()->create([
-            'nome'=> 'Pendente',
-            'descricao' => 'PendenteDesc',
-        ]);
-        StatusCotacao::factory()->create([
-            'nome'=> 'Concluído',
-            'descricao' => 'ConcluídoDesc',
-        ]);
-        CategoriaBar::factory()->create([
-            'nome'=> 'Bar 1',
-            'descricao' => 'Bar 1Desc',
-        ]);
-        CategoriaBar::factory()->create([
-            'nome'=> 'Bar 2',
-            'descricao' => 'Bar 2Desc',
-        ]);
-        CategoriaBar::factory()->create([
-            'nome'=> 'Bar 3',
-            'descricao' => 'Bar 3Desc',
-        ]);
-        CategoriaPost::factory()->create([
-            'nome'=> 'ambiente',
-            'descricao' => 'BarDesc',
-        ]);
-        CategoriaPost::factory()->create([
-            'nome'=> 'galeria',
-            'descricao' => 'DrinkDesc',
-        ]);
-        CategoriaPost::factory()->create([
-            'nome'=> 'cardapio',
-            'descricao' => 'EventoDesc',
-        ]);
-        CategoriaEvento::factory()->create([
-            'nome'=> 'Evento 1',
-            'descricao' => 'Evento 1Desc',
-        ]);
-        CategoriaEvento::factory()->create([
-            'nome'=> 'Evento 2',
-            'descricao' => 'Evento 2Desc',
-        ]);
-        CategoriaEvento::factory()->create([
-            'nome'=> 'Evento 3',
-            'descricao' => 'Evento 3Desc',
-        ]);
-        Cotacao::factory()->create([
-            'nome'=> 'João',
-            'data_evento' => now(),
-            'telefone' => '71 - 9 9999-9999',
-            'email' => 'joao@example.com',
-            'localidade' => 'Rio de Janeiro',
-            'observacoes' => 'Teste de cotação',
-            'qtd_convidados' => '20',
-            'status_cotacao_id' => '1',
-            'categoria_bar_id' => '1',
-            'categoria_evento_id' => '1'
-        ]);
-        Cotacao::factory()->create([
-            'nome'=> 'João',
-            'data_evento' => now(),
-            'telefone' => '71 - 9 9999-9999',
-            'email' => 'joao@example.com',
-            'localidade' => 'Rio de Janeiro',
-            'observacoes' => 'Teste de cotação',
-            'qtd_convidados' => '20',
-            'status_cotacao_id' => '2',
-            'categoria_bar_id' => '1',
-            'categoria_evento_id' => '1'
-        ]);
-        Cotacao::factory()->create([
-            'nome'=> 'João',
-            'data_evento' => now(),
-            'telefone' => '71 - 9 9999-9999',
-            'email' => 'joao@example.com',
-            'localidade' => 'Rio de Janeiro',
-            'observacoes' => 'Teste de cotação',
-            'qtd_convidados' => '20',
-            'status_cotacao_id' => '3',
-            'categoria_bar_id' => '1',
-            'categoria_evento_id' => '1'
-        ]);
-        Curriculo::factory()->create([
-            'nome' => 'João',
-            'arquivo' => 'Silva',
-            'informacoes' => '30',
-            'profissao_id' => '1'
-        ]);
-
-        
-
-        Post::factory()->create([            
-            'titulo' => '2 Limões',
-            'descricao' => 'Base de limão siciliano, limão tahiti, melaço de cana e cachaça.',
-            'visibilidade'=> true,
-            'arquivo' => 'drink1.jpg',
-            'categoria_id' => 2,
-        ]);
-
-        Post::factory()->create([
-            'titulo' => '43 Sour ',
-            'descricao' => 'Base de licor 43 e sumo de limão siciliano.',
-            'visibilidade'=> true,
-            'arquivo' => 'drink2.jpg',
-            'categoria_id' => 2,
-        ]);
-
+        for($i = 0; $i <100; $i++) {
+            Cotacao::factory()->create([
+                'nome'=> fake()->name(),
+                'data_evento' => fake()->date(),
+                'telefone' => fake()->phoneNumber(),
+                'email' => fake()->email(),
+                'localidade' => fake()->streetAddress(),
+                'observacoes' => fake()->text(),
+                'qtd_convidados' => fake()->numberBetween(0,50),
+                'status_cotacao_id' => fake()->numberBetween(1,3),
+                'categoria_bar_id' => fake()->numberBetween(1,3),
+                'categoria_evento_id' => fake()->numberBetween(1,3)
+            ]);
+            Post::factory()->create([            
+                'titulo' => fake()->title(),
+                'descricao' => fake()->text(),
+                'visibilidade'=> fake()->boolean(),
+                'arquivo' => fake()->text(),
+                'categoria_id' => fake()->numberBetween(1,3)
+            ]);
+            Profissao::factory()->create([
+                'nome' => fake()->jobTitle(),
+                'descricao' => fake()->text()
+            ]);
+        }
+        for($i = 0; $i <100; $i++) {
+            Curriculo::factory()->create([
+                'nome' => fake()->name(),
+                'arquivo' => fake()->name(),
+                'informacoes' => fake()->text(),
+                'profissao_id' => fake()->numberBetween(1,3)
+            ]);
+        }
 
     }
 }
---
+++ database/seeders/KeysSeeder.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Profissao;
+use App\Models\StatusCotacao;
+use App\Models\CategoriaBar;
+use App\Models\CategoriaEvento;
+use App\Models\CategoriaPost;
+
+class KeysSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Profissao::factory()->create([
+            'nome'=> 'Bartender',
+            'descricao' => 'BartenderDesc',
+        ]);        
+        Profissao::factory()->create([
+            'nome'=> 'Backbar',
+            'descricao' => 'BackbarDesc',
+        ]);
+        Profissao::factory()->create([
+            'nome'=> 'Copeira',
+            'descricao' => 'CopeiraDesc',
+        ]);
+        Profissao::factory()->create([
+            'nome'=> 'Garçom',
+            'descricao' => 'GarçomDesc',
+        ]);
+        StatusCotacao::factory()->create([
+            'nome'=> 'Novo',
+            'descricao' => 'NovoDesc',
+        ]);
+        StatusCotacao::factory()->create([
+            'nome'=> 'Pendente',
+            'descricao' => 'PendenteDesc',
+        ]);
+        StatusCotacao::factory()->create([
+            'nome'=> 'Concluído',
+            'descricao' => 'ConcluídoDesc',
+        ]);
+        CategoriaBar::factory()->create([
+            'nome'=> 'Bar 1',
+            'descricao' => 'Bar 1Desc',
+        ]);
+        CategoriaBar::factory()->create([
+            'nome'=> 'Bar 2',
+            'descricao' => 'Bar 2Desc',
+        ]);
+        CategoriaBar::factory()->create([
+            'nome'=> 'Bar 3',
+            'descricao' => 'Bar 3Desc',
+        ]);
+        CategoriaPost::factory()->create([
+            'nome'=> 'ambiente',
+            'descricao' => 'BarDesc',
+        ]);
+        CategoriaPost::factory()->create([
+            'nome'=> 'galeria',
+            'descricao' => 'DrinkDesc',
+        ]);
+        CategoriaPost::factory()->create([
+            'nome'=> 'cardapio',
+            'descricao' => 'EventoDesc',
+        ]);
+        CategoriaEvento::factory()->create([
+            'nome'=> 'Evento 1',
+            'descricao' => 'Evento 1Desc',
+        ]);
+        CategoriaEvento::factory()->create([
+            'nome'=> 'Evento 2',
+            'descricao' => 'Evento 2Desc',
+        ]);
+        CategoriaEvento::factory()->create([
+            'nome'=> 'Evento 3',
+            'descricao' => 'Evento 3Desc',
+        ]);
+
+    }
+}
